814099e9ac6c387878dff17da4e12fbc9239c5bd
Chanon Rattanajaratkul <chanon45559@outlook.com>
2022-10-23 22:37:28 +0700

    Update comment

--- to_submit/SVD.py
+++ to_submit/SVD.py
@@ -1,14 +1,17 @@
 # edit file path
 REF = 'ref.txt'
 READ = 'read.txt'
-DEBUG_MODE = False #True/False
+DEBUG_MODE = False # True/False
 
 with open(REF) as f:
     ref = f.read().splitlines()[0]
 with open(READ) as f:
     reads = f.read().splitlines()
+if (DEBUG_MODE):
+    print(ref)
 
 def get_score(read, ref_part):
+    # calculate score for 1 read and 1 position of ref 
     score = 0
     for i in range(len(ref_part)):
         if read[i] == ref_part[i]:
@@ -16,6 +19,7 @@ def get_score(read, ref_part):
     return score
 
 def read_mapping(read, ref):
+    # perform read_mapping for 1 read
     max_score, best_idx = (-1, -1)
     for i in range(len(ref) - len(read) + 1):
         score = get_score(read, ref[i:i+len(read)])
@@ -54,17 +58,15 @@ def get_mapping_type(read1, read1_idx, is_read1_inv, read2, read2_idx, is_read2_
     read1 reverse = [  r >
     read2 = <    ]
     read2 reverse = <  r ]
-    normal length space between read1 and read2 = ---
-    short length space between read1 and read2 = -
-    long length space between read1 and read2 = ------
-    any length space between read1 and read2 = -?-
+    normal distance between read1 and read2 = ---
+    short distance between read1 and read2 = -
+    long distance between read1 and read2 = ------
     mapped = <    ]
     soft-clipped = <  ##]
     unmapped = <####]
-    any = <  ? ]
 
     type:
-    B  = <    ]-?-[    >
+    B  = <    ]-?-[    > 
     I  = [  r >-?-<  r ] || [    >-?-<  r ] || [  r >-?-<   ]
     S  = [    >-<    ]
     L  = [    >------<    ]
@@ -77,29 +79,29 @@ def get_mapping_type(read1, read1_idx, is_read1_inv, read2, read2_idx, is_read2_
     '''
     p1 = get_percent_match(read1,ref[read1_idx:read1_idx+len(read1)],is_read1_inv)
     p2 = get_percent_match(read2,ref[read2_idx:read2_idx+len(read2)],is_read2_inv)
-    mapped_threshold = 0.85
-    unmapped_threshold = 0.6
+    MAPPED_THRESHOLD = 0.85
+    UNMAPPED_THRESHOLD = 0.6
 
-    if (p1 >= unmapped_threshold or p2 >= unmapped_threshold):
+    if (p1 >= UNMAPPED_THRESHOLD or p2 >= UNMAPPED_THRESHOLD):
         if (read2_idx <= read1_idx):
-            return "B"
+            return "B" # Read2 before Read1
         if (is_read1_inv or is_read2_inv):
-            return "I"
+            return "I" # Inversion
         if (read2_idx - read1_idx < len(read1) + normal_space):
-            return "S"
+            return "S" # Short
         if (read2_idx - read1_idx > len(read1) + normal_space):
-            return "L"
-        if (p1 >= mapped_threshold and p2 >= mapped_threshold):
-            return "M"
-        if (p1 >= mapped_threshold and p2 >= unmapped_threshold):
+            return "L" # Long
+        if (p1 >= MAPPED_THRESHOLD and p2 >= MAPPED_THRESHOLD):
+            return "M" # Mapped
+        if (p1 >= MAPPED_THRESHOLD and p2 >= UNMAPPED_THRESHOLD):
             return "R1"
-        if (p1 >= mapped_threshold and p2 < unmapped_threshold):
+        if (p1 >= MAPPED_THRESHOLD and p2 < UNMAPPED_THRESHOLD):
             return "R2"
-        if (p1 >= unmapped_threshold and p2 >= mapped_threshold):
+        if (p1 >= UNMAPPED_THRESHOLD and p2 >= MAPPED_THRESHOLD):
             return "L1"
-        if (p1 < unmapped_threshold and p2 >= mapped_threshold):
+        if (p1 < UNMAPPED_THRESHOLD and p2 >= MAPPED_THRESHOLD):
             return "L2"
-    return "U"
+    return "U" # Unmapped
 
 def visualize_read_mapping(read1, is_read1_inv, read1_idx, read1_score, read2, is_read2_inv, read2_idx, read2_score, type):
     if not is_read1_inv:
@@ -112,8 +114,8 @@ def visualize_read_mapping(read1, is_read1_inv, read1_idx, read1_score, read2, i
         print(' '*read2_idx + read2[::-1] + ' /IR2 /T=' + type + ' /S=' + str(read2_score))
 
 def get_reads_maping_data(reads, ref):
-    data_list = [] 
-    space_list = []
+    data_list = [] # list of [ (read1,is_read1_inv,read1_idx,read1_score) , (read2,is_read2_inv,read2_idx,read2_score) ]
+    space_list = [] # list of distance between read1 and read 2
     for line in reads:
         read1, read2 = line.split(',')
 
@@ -131,14 +133,17 @@ def get_reads_maping_data(reads, ref):
         read1_score = read1_inv_map[0] if (read1_inv_map[0] > read1_map[0]) else read1_map[0]
         read2_score = read2_inv_map[0] if (read2_inv_map[0] > read2_map[0]) else read2_map[0]
 
+        # calculate distance (space) between read1 and read 2 
         space = read2_idx - read1_idx - len(read1)
         space_list.append(space)
         data_list.append([ (read1,is_read1_inv,read1_idx,read1_score) , (read2,is_read2_inv,read2_idx,read2_score) ])
-    return data_list, max(set(space_list), key=space_list.count) #normal length space
+    return data_list, max(set(space_list), key=space_list.count) #normal distance = the most frequent number
 
 def get_type_count(data_list, normal_space, ref):
+    # find and count type from every reads
     type_count = {"B":0,"I":0,"S":0,"L":0,"M":0,"R1":0,"R2":0,"L1":0,"L2":0,"U":0}
     breakpoint = {"R":[], "L":[]}
+
     for (data_read1, data_read2) in data_list:
         read1, is_read1_inv, read1_idx, read1_score = data_read1[0], data_read1[1], data_read1[2], data_read1[3]
         read2, is_read2_inv, read2_idx, read2_score = data_read2[0], data_read2[1], data_read2[2], data_read2[3]
@@ -158,6 +163,7 @@ def get_type_count(data_list, normal_space, ref):
     return (type_count, breakpoint)
 
 def get_SV(type_count, breakpoint, ref):
+    # find SV from type_count and breakpoint
     breakpoint_R = max(set(breakpoint["R"]), key=breakpoint["R"].count, default=len(ref))
     breakpoint_L = max(set(breakpoint["L"]), key=breakpoint["L"].count, default=len(ref))
 
@@ -184,8 +190,6 @@ def get_SV(type_count, breakpoint, ref):
 
     return "Unknown SV"
 
-if (DEBUG_MODE):
-    print(ref)
 data_list, normal_space = get_reads_maping_data(reads, ref)
 type_count, breakpoint = get_type_count(data_list, normal_space, ref)
 print(get_SV(type_count, breakpoint, ref))
